30d485db8dfdd1eeb0df557119ec96093aa33298
Kiernan809 <lee.kiernan@gmail.com>
2015-04-24 19:51:44 +0100

    Re-init

---
+++ lib/teamwork.rb
@@ -0,0 +1,59 @@
+require "teamwork/version"
+require 'faraday'
+require 'faraday_middleware'
+require 'json'
+
+module Teamwork
+
+  class API
+    def initialize(company, api_key)
+      @company = company
+      @api_key = api_key
+      @connection = Faraday.new url: "http://#{company}.teamwork.com/" do |con|
+        con.request :multipart
+        con.request :json
+        con.request :url_encoded
+        con.response :json, content_type: /\bjson$/
+        con.adapter :net_http
+
+        con.headers[:cache_control] = 'no-cache'
+        con.basic_auth(api_key, '')
+      end
+    end
+
+    def activity(params:{})
+      response = @connection.get "latestActivity.json", params
+      response.body
+    end
+
+    def taskList(id, params:{})
+      response = @connection.get "todo_lists/#{id}.json", params
+      response.body
+    end
+
+    def projectTaskLists(id, params:{})
+      response = @connection.get "projects/#{id}/todo_lists.json", params
+      response.body
+    end
+
+    # GET /tasks/{id}.json
+    def task(id, params:{})
+      response = @connection.get "tasks/#{id}.json", params
+      response.body
+    end
+
+    # GET /tasklists/{id}/tasks.json
+    def taskListTasks(id, params:{})
+      response = @connection.get "tasklists/#{id}/tasks.json", params
+      response.body
+    end
+
+
+    def person(id, params:{})
+      response = @connection.get "people/#{id}.json", params
+      response.body
+    end
+
+    # TODO: the rest...
+  end
+end
---
+++ lib/teamwork/version.rb
@@ -0,0 +1,3 @@
+module Teamwork
+  VERSION = "0.1.2"
+end
